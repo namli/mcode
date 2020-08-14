9506391bb34110e196b4e2287d83ee659157c176
namli <namli@mail.ru>
2020-08-14 15:33:54 +0200

    week_4_hw_complite

--- exercises-js1/week-1/Homework/F-strings-methods/exercise.js
+++ exercises-js1/week-1/Homework/F-strings-methods/exercise.js
@@ -1,3 +1,3 @@
 // Start by creating a variable `message`
-
+let message = "Hello World";
 console.log(message);
--- exercises-js1/week-1/Homework/K-functions-parameters/exercise4.js
+++ exercises-js1/week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,8 @@
 // Declare your function first
-
+function sum(a, b) {
+  return a + b;
+}
+let c = sum(2, 3);
 // Call the function and assign to a variable `sum`
 
-console.log(sum);
+console.log(c);
