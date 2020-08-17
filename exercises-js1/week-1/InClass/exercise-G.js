2ec2cbe00b1711fc4bf36777f494151494afb8d2
namli <namli@mail.ru>
2020-08-17 09:32:13 +0200

    small fixes

--- exercises-js1/week-1/InClass/exercise-G.js
+++ exercises-js1/week-1/InClass/exercise-G.js
@@ -1,7 +1,26 @@
-let numberOfStudents = 10, numberOfMentors = 5;
+let numberOfStudents = 10;
+let numberOfMentors = 5;
 
-let a = numberOfStudents + numberOfMentors;
-let pocOfStudent = Math.round(numberOfStudents / a * 100);
-let pocOfMentor = Math.round(numberOfMentors / a * 100);
-console.log(`Percentage students: ${pocOfStudent}%`);
-console.log(`Percentage mentors: ${pocOfMentor}%`);
+
+/**
+ *  Count procent of first number(a) from summ of two number(a+b) 
+ */
+function countProcents(a, b) {
+  let procent = Math.round(a / (a + b) * 100);
+  return procent;
+}
+/**
+ * Generate message and call countProcents from inside.
+ * 
+ */
+function getMessage(text, numOne, numTwo) {
+  let procent = countProcents(numOne, numTwo);
+  let message = text + procent + '%';
+  return message;
+
+}
+let text1 = getMessage('Percentage students:', numberOfStudents, numberOfMentors);
+console.log(text1);
+
+let text2 = getMessage('Percentage mentors:', numberOfMentors, numberOfStudents);
+console.log(text2);
