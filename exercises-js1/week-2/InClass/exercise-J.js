b9d9420480064a540acedccbd62b49ee9d346fef
namli <namli@mail.ru>
2020-08-18 20:03:04 +0200

    week5_class_doing

--- exercises-js1/week-2/InClass/exercise-J.js
+++ exercises-js1/week-2/InClass/exercise-J.js
@@ -1,6 +1,12 @@
+const students = ["Ahmed", "Maria", "Atanas", "Nahidul", "Jack"];
+const studentsa = ["Ahmed", "Amy", "Atanas", "Nahidul", "Jack"];
+
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1] == "Amy") {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+
+console.log(secondMatchesAmy(students));
+console.log(secondMatchesAmy(studentsa));
---
+++ exercises-js1/week-2/InClass/exercise-K.js
@@ -0,0 +1,10 @@
+const students = ["Ahmed", "Maria", "Atanas", "Nahidul", "Jack"];
+const studentsa = ["Ahmed", "Amy", "Atanas", "Nahidul", "Jack"];
+
+students.forEach(element => {
+  console.log(element);
+});
+
+studentsa.forEach(element => {
+  console.log(element);
+});
