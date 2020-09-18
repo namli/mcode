934decfd79977c99bdbf7c3bf27a678dfcfd7f12
namli <namli@mail.ru>
2020-09-19 01:31:38 +0200

    HomwWork9

--- exercises-js2/week-3/Homework/1-alarmclock/alarmclock.js
+++ exercises-js2/week-3/Homework/1-alarmclock/alarmclock.js
@@ -1,4 +1,46 @@
-function setAlarm() {}
+var state;
+function setAlarm() {
+  time = document.getElementById('alarmSet').valueAsNumber;
+  indicator = document.getElementById('timeRemaining');
+  state = 1;
+  indicatorText = indicator.innerText.split(' ');
+  indicatorText = indicatorText[0] + ' ' + indicatorText[1];
+  timeToAlarm = time;
+
+  let timer = setInterval(() => {
+    if (timeToAlarm === 0) {
+      clearInterval(timer);
+      playAlarm();
+      state = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
+    if (state != 2) {
+      console.log(state);
+      indicator.innerText = indicatorText + ' ' + new Date(timeToAlarm * 1000).toISOString().substr(11, 8);
+      timeToAlarm = timeToAlarm - 1;
+    }
+  }, 1000);
+
+  document.getElementById("stop").addEventListener("click", () => {
+    document.querySelector('body').style.backgroundColor = 'white';
+    state = 0;
+    document.getElementById('alarmSet').value = '';
+    clearInterval(timer);
+    indicator.innerText = indicatorText + ' 00:00';
+  });
+
+  document.getElementById("pause").addEventListener("click", (e) => {
+    console.log(state);
+    if (state === 1) {
+      state = 2;
+      e.target.innerText = 'Play';
+    } else if (state === 2) {
+      state = 1;
+      e.target.innerText = 'Pause';
+    }
+  });
+}
+
 
 // DO NOT EDIT BELOW HERE
 
--- exercises-js2/week-3/InClass/B-movies/exercise.js
+++ exercises-js2/week-3/InClass/B-movies/exercise.js
@@ -8,8 +8,11 @@ Create a function called "showMovies" that
 - for each movie, it creates a <p> element with the movie title and director and append it to the #all-movies div.
 - it sets the innerText of the #movies-number element to the total number of the movies in the array "movies"
 
+
+
+
 Task 2
-Amend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that
+Atend your function above to only show movies after 1 second. Remember to use setTimeout to achieve that
 Create a new function called "addMovie"
 - it receives a movie object as an argument - your can create a new object for your favorite movie following using the "myMovies" objects as a guide 
 - it adds the new movie to the list of movies after 2 seconds. Remember to setTimeout to achieve that
@@ -58,10 +61,53 @@ var movies = [
   },
 ];
 
+movieList = document.querySelector('#all-movies');
+movieNum = document.querySelector('#movies-number');
+mContainer = document.createElement("div.movielist");
 // create showMovies function
+function showMovies() {
+  movieNum.innerText = 0;
+  let i = 0;
+  mContainer.innerText = '';
+  movieList.appendChild(mContainer);
+  movies.forEach(el => {
+    setTimeout(function (el) {
+      p = document.createElement('p');
+      p.innerText = `${el.title} - ${el.director}`;
+      mContainer.appendChild(p);
+      movieNum.innerText = parseInt(movieNum.innerText) + 1;
+    }, 1000 * i++, el);
+  });
+}
 
 
-// create a new movie object for your favorite movie
-
+//create a new movie object for your favorite movie
+let myMovie = {
+  title: "Fear and loathing in las vegas",
+  director: "Terry Gilliam",
+  type: "Adventure",
+  haveWatched: false,
+};
 
 // create addMovies function
+
+function addMovies(movie, func) {
+  movies.push(movie);
+  func();
+}
+
+addMovies(myMovie, showMovies);
+
+document.addEventListener('click', addMovieFromForm);
+
+function addMovieFromForm(e) {
+  if (e.target.id === "movie-formsubmit") {
+    let movie = {};
+    movie.name = document.querySelector('#movie-title').value;
+    movie.director = document.querySelector('#movie-director').value;
+    movie.type = document.querySelector('#movie-type').value;
+    movie.haveWatched = document.querySelector('#movie-haveWatched').checked;
+
+    addMovies(movie, showMovies);
+  }
+}
